2d635874b0311c100a209247e921c850e26e7c6a
AndreyB7 <0129507@gmail.com>
2021-11-16 18:31:55 +0700

    api cart object DTO update

--- pages/api/checkout_sessions/cart.ts
+++ pages/api/checkout_sessions/cart.ts
@@ -8,16 +8,20 @@ import { NextApiRequest, NextApiResponse } from 'next'
  * The important thing is that the product info is loaded from somewhere trusted
  * so you know the pricing information is accurate.
  */
-const validateCartItems = require('use-shopping-cart/src/serverUtil')
-  .validateCartItems
+import { validateCartItems } from 'use-shopping-cart/src/serverUtil'
 import inventory from '../../../data/products.json'
 
 import Stripe from 'stripe'
+import { CartEntry, Product } from 'use-shopping-cart'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   // https://github.com/stripe/stripe-node#configuration
   apiVersion: '2020-08-27',
 })
+
+const formatBody = (cartItems: CartEntry) => {
+  return cartItems.reduce((ci: CartEntry, p: Product) => ({ ...ci, [p.sku]: p}), {})
+}
 
 export default async function handler(
   req: NextApiRequest,
@@ -26,8 +30,8 @@ export default async function handler(
   if (req.method === 'POST') {
     try {
       // Validate the cart details that were sent from the client.
-      const cartItems = req.body
-      
+      const cartItems = formatBody(req.body)
+      console.log(cartItems);
       // remove images from Inventory for Stripe object
       let inventoryNoImages = inventory.map(({ image, ...rest }) => rest);
 
@@ -53,3 +57,16 @@ export default async function handler(
     res.status(405).end('Method Not Allowed')
   }
 }
+// {
+//     "sku_cource_1": {
+//         "name": "Course Name First",
+//         "sku": "sku_cource_1",
+//         "price": 220,
+//         "image": "/cource-placeholder@4x.jpg",
+//         "currency": "USD",
+//         "id": "sku_cource_1",
+//         "quantity": 1,
+//         "value": 220,
+//         "formattedValue": "US$2.20"
+//     }
+// }
