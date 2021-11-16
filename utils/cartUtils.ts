b91a5aceb69cf6eb98c51e7e9e3979deb40eb43a
AndreyB7 <0129507@gmail.com>
2021-11-16 21:06:14 +0700

    types error fix

--- utils/cartUtils.ts
+++ utils/cartUtils.ts
@@ -1,20 +1,23 @@
 export interface Product {
-	section_id: number
+	section_id: string
 	section_name: string
 	section_desc: string
 	price: number
 	course_image: string
 	currency: string
+  price_data?: {
+    product_data: object
+  }
 }
 
-export const validateCartItems = (inventorySrc, cartDetails) => {
+export const validateCartItems = (inventorySrc: Product[], cartDetails: { [id: string]: any }) => {
   const validatedItems = []
 
   for (const itemId in cartDetails) {
     const product = cartDetails[itemId]
     const inventoryItem = inventorySrc.find(
       (currentProduct) =>
-        currentProduct.section_id === itemId || currentProduct.section_id === itemId
+        currentProduct.section_id === itemId
     )
 
     if (!inventoryItem) throw new Error(`Product ${itemId} not found!`)
@@ -24,38 +27,18 @@ export const validateCartItems = (inventorySrc, cartDetails) => {
         currency: inventoryItem.currency,
         unit_amount: inventoryItem.price * 100, // convert price to cents
         product_data: {
-          name: inventoryItem.section_name
+          name: inventoryItem.section_name,
+          images: ['']
         }
       },
       quantity: product.quantity ? product.quantity : 1
     }
-    if (inventoryItem.description)
-      item.price_data.product_data.description = inventoryItem.description
-    if (inventoryItem.image)
-      item.price_data.product_data.images = [inventoryItem.image]
-    if (inventoryItem.price_data)
-      item.price_data = {
-        ...item.price_data,
-        ...inventoryItem.price_data
-      }
-    if (inventoryItem.product_data)
-      item.price_data.product_data = {
-        ...item.price_data.product_data,
-        ...inventoryItem.product_data
-      }
+    
+    if (inventoryItem.course_image)
+      item.price_data.product_data.images = [inventoryItem.course_image]
+
     validatedItems.push(item)
   }
 
   return validatedItems
-}
-
-export const formatLineItems = (cartDetails) => {
-  const lineItems = []
-
-  for (const itemId in cartDetails) {
-    if (cartDetails[itemId].sku_id || cartDetails[itemId].price_id)
-      lineItems.push({ price: itemId, quantity: cartDetails[itemId].quantity })
-  }
-
-  return lineItems
 }
